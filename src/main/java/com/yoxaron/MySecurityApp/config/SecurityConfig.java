b72e14aeeabdddfed177b65392be3947b64c713b
yoxaron <dumbbeliever@gmail.com>
2023-10-11 21:21:08 +0100

    Removed custom AuthProviderImpl

--- src/main/java/com/yoxaron/MySecurityApp/config/SecurityConfig.java
+++ src/main/java/com/yoxaron/MySecurityApp/config/SecurityConfig.java
@@ -1,32 +1,35 @@
 package com.yoxaron.MySecurityApp.config;
 
-import com.yoxaron.MySecurityApp.security.AuthProviderImpl;
+import com.yoxaron.MySecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-//@EnableWebSecurity
-public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
-    private final AuthProviderImpl authProvider;
+public class SecurityConfig {
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
-
-//    protected void configure(AuthenticationManagerBuilder auth) {
-//        auth.authenticationProvider(authProvider);
-//    }
 
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
