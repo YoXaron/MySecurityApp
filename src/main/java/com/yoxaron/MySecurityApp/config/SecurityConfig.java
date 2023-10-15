bc545df1c319983eb639e66a11311a2867190a82
yoxaron <dumbbeliever@gmail.com>
2023-10-15 22:29:18 +0100

    Add password encryption

--- src/main/java/com/yoxaron/MySecurityApp/config/SecurityConfig.java
+++ src/main/java/com/yoxaron/MySecurityApp/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,8 +24,6 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
-
-    //Spring Security 6
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -53,12 +51,15 @@ public class SecurityConfig {
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(personDetailsService);
+
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
+
         return authenticationManagerBuilder.build();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/yoxaron/MySecurityApp/services/RegistrationService.java
+++ src/main/java/com/yoxaron/MySecurityApp/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.yoxaron.MySecurityApp.services;
 import com.yoxaron.MySecurityApp.models.Person;
 import com.yoxaron.MySecurityApp.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PersonRepository personRepository) {
+    public RegistrationService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+
         personRepository.save(person);
     }
 }
